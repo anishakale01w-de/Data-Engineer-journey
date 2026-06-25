5d8213f51c1c20ee12d4c49dc48a9890857fb22d
Anisha Kale <anishakale01w@gmail.com>
2026-06-26 03:18:20 +0530

    Refactor SQL query for second highest salary
    Updated SQL query to find the second highest salary and included an outer query to retrieve employee names.

--- sql/Interview_questions/2nd_highest_salary.sql
+++ sql/Interview_questions/2nd_highest_salary.sql
@@ -3,4 +3,10 @@ Question 1 — Second highest salary
 Table: employees (emp_id, emp_name, department, salary)
 Write a query to find the second highest salary.
 */
-SELECT emp_id, emp_name FROM employee_tbl where salary <> max(salary);
+SELECT max(salary) FROM employee_tbl where salary <(select  max(salary) from employee_tbl);
+
+-- incase name also asked add one more outer query and make this subquery
+
+SELECT emp_id, emp_name from emp_tbl where salary = (SELECT max(salary) FROM employee_tbl 
+  where salary <(select  max(salary) from employee_tbl) 
+  );
